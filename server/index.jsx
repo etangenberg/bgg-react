401166eb1fc3c3f5d3186a934f319e4a488c9153
Edwin Tangenberg <etangenberg@hotmail.com>
2018-03-23 15:43:21 +0100

    First Commit

---
+++ server/index.jsx
@@ -0,0 +1,21 @@
+import express from 'express';
+import yields from 'express-yields';
+import fs from 'fs-extra';
+import webpack from 'webpack';
+
+const port = process.env.PORT || 3000;
+const app = new express();
+
+if(process.env.NODE_ENV === 'development'){
+    const config = require('../webpack.config.dev.babel').default;
+    const compiler = webpack(config);
+    
+    app.use(require('webpack-dev-middleware')(compiler, {noinfo: true}));
+    app.use(require('webpack-hot-middleware')(compiler));
+}
+
+app.get(['/'], function * (req,res) {
+        let index = yield fs.readFile('./public/index.html',"utf-8");
+        res.send(index);
+        });
+app.listen(port, '0.0.0.0', () => console.info(`App listening on ${port}`));
